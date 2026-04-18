bd1dd1b5b5251b6e929f9aa31a586243dd66849d
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-18 17:29:29 +0300

    add auth interfaces to Application layer

---
+++ Learnix.Backend/Learnix.Application/Common/Interfaces/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace Learnix.Application.Common.Interfaces;
+
+public interface IEmailSender
+{
+    Task SendEmailConfirmationAsync(string toEmail, string firstName, string confirmationLink, CancellationToken ct = default);
+    Task SendPasswordResetAsync(string toEmail, string firstName, string resetLink, CancellationToken ct = default);
+}
