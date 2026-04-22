3196b5c23b79cb1d103acee9914dbd78c54fdc8b
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-23 01:29:43 +0300

    update reositories

---
+++ Learnix.Backend/Learnix.Infrastructure/Persistence/Repositories/LessonRepository.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Learnix.Application.Lessons.Abstractions;
+using Learnix.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learnix.Infrastructure.Persistence.Repositories;
+
+internal sealed class LessonRepository(ApplicationDbContext context)
+    : RepositoryBase<Lesson>(context), ILessonRepository
+{
+    public Task<T?> GetLessonOfTypeByIdAsync<T>(Guid id, bool forUpdate = false, CancellationToken ct = default)
+            where T : Lesson
+    {
+        var query = context.Set<Lesson>().OfType<T>();
+
+        if (!forUpdate)
+            query = query.AsNoTracking();
+
+        return query.FirstOrDefaultAsync(l => l.Id == id, ct);
+    }
+
+    public Task<int> GetMaxDisplayOrderAsync(Guid sectionId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ShiftLessonsOrderAsync(Guid sectionId, Guid lessonId, int newOrder, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Learnix.Backend/Learnix.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
+++ Learnix.Backend/Learnix.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -1,18 +1,10 @@
-﻿using Learnix.Application.Auth.Abstractions;
-using Learnix.Application.Common.Specifications;
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Learnix.Application.Auth.Abstractions;
 using Learnix.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace Learnix.Infrastructure.Persistence.Repositories;
 
-internal sealed class RefreshTokenRepository(ApplicationDbContext context) : IRefreshTokenRepository
+internal sealed class RefreshTokenRepository(ApplicationDbContext context)
+    : RepositoryBase<RefreshToken>(context), IRefreshTokenRepository
 {
-    public Task<RefreshToken?> FirstOrDefaultAsync(Specification<RefreshToken> spec, CancellationToken ct = default)
-        => SpecificationEvaluator<RefreshToken>.GetQuery(context.RefreshTokens, spec).FirstOrDefaultAsync(ct);
-
-    public Task<List<RefreshToken>> ListAsync(Specification<RefreshToken> spec, CancellationToken ct = default)
-        => SpecificationEvaluator<RefreshToken>.GetQuery(context.RefreshTokens, spec).ToListAsync(ct);
-
-    public async Task AddAsync(RefreshToken refreshToken, CancellationToken ct = default)
-        => await context.RefreshTokens.AddAsync(refreshToken, ct);
 }
