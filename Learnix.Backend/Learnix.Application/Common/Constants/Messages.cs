d4cd89168b3aaca0e2c322b7e752f0cfb704bab4
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-27 18:55:46 +0300

    add instructor application features

--- Learnix.Backend/Learnix.Application/Common/Constants/Messages.cs
+++ Learnix.Backend/Learnix.Application/Common/Constants/Messages.cs
@@ -7,6 +7,7 @@ internal static class CommonMessages
     internal static string CourseNotFound(Guid courseId) => $"Course {courseId} not found.";
     internal static string SectionNotFound(Guid sectionId) => $"Section {sectionId} not found.";
     internal static string LessonNotFound(Guid lessonId) => $"Lesson {lessonId} not found.";
+    internal static string InstructorApplicationNotFound(Guid applicationId) => $"Application {applicationId} not found.";
     internal static string CourseCategoryNotFound(Guid categoryId) => $"Course category '{categoryId}' not found.";
     internal static string NotOwnerOfCourse => "You are not the owner of this course.";
     internal static string OnlyAdminCanManageCategories => "Only admins can manage categories.";
